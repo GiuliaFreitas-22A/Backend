3408ea85a1b4a2c6e305bac2497f946cfe4c868c
GiuliaFreitas-22A <giulia.fsantos@eaportal.org>
2025-10-22 23:41:00 -0300

    Backend

---
+++ src/persons.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        name: "Nicole Evangelista",
+        email: "evangelistanicole@gmail.com",
+        age: 12,
+        nickname: "nicolekawaii123"
+    },
+    {
+        name: "Kamilla Ribeiro",
+        email: "ribeirinhakamilla@gmail.com",
+        age: 30,
+        nickname: "kamilla123"
+    },
+    {
+        name: "Giulia Freitas",
+        email: "giulialindamaravilhosa@gmail.com",
+        age: 16,
+        nickname: "giulinda"
+    },
+    {
+        name: "Gabriel Scuotegazza Portilho",
+        email: "representantemagnanimosoberanosupremo@gmail.com",
+        age: 70,
+        nickname: "gaybriel"
+    },
+]
+
+export { persons }
